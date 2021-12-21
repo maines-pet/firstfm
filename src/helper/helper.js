375cb3d1a7b0072a828f4f3ef76b749be3f35e78
kevin.manalili <ksmanalili@gmail.com>
2021-12-21 16:03:42 +1100

    Change duration conversion format

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,5 +1,5 @@
 export function convertMiliSecToMinSec(duration) {
   const min = Math.floor(duration / (1000 * 60))
   const sec = (duration / 1000) % 60
-  return min + 'm' + sec.toString().padStart(2, '0') + 's'
+  return min.toString().padStart(2, 0) + ':' + sec.toString().padStart(2, '0')
 }
